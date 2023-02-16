d3d596e5f878341d9613f8e862d05943762793ea
Yassin Sultan <96452651+YassinSultan@users.noreply.github.com>
2023-02-17 00:44:43 +0200

    uploading project files

---
+++ js/myscript.js
@@ -0,0 +1,31 @@
+let thumbs = document.querySelectorAll(".thumbs ul li")
+let circle = document.querySelector(".circle")
+let mainImg = document.querySelector(".mainImg")
+function changeCircle(color){
+    document.documentElement.style.setProperty('--main-color', `${color}`);
+}
+function changeImg(src){
+    setTimeout(() => {
+        mainImg.src=src
+    }, 200);
+}
+function changeThumbs(thumb){
+    thumbs.forEach(thumb2=>{
+        if(thumb2.classList="active"){
+            thumb2.classList.remove("active")
+        }
+    })
+    thumb.classList.add("active")
+}
+thumbs.forEach(thumb => {
+    thumb.addEventListener("click",()=>{
+        changeThumbs(thumb)
+        changeCircle(thumb.dataset.color)
+        changeImg(thumb.dataset.src)
+    })
+});
+//change navbar color
+if (window.innerWidth < 767.98){
+    let navbar = document.querySelector(".navbar")
+    navbar.classList.add("navbar-dark")
+}
