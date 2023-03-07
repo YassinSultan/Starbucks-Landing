247bb5cae8a79736ad2aed5af3f86732d18e8118
Yassin Sultan <96452651+YassinSultan@users.noreply.github.com>
2023-03-07 21:12:43 +0200

    modified in javascript media query

--- js/myscript.js
+++ js/myscript.js
@@ -1,32 +1,33 @@
 let thumbs = document.querySelectorAll(".thumbs ul li")
 let circle = document.querySelector(".circle")
 let mainImg = document.querySelector(".mainImg")
-function changeCircle(color){
+function changeCircle(color) {
     document.documentElement.style.setProperty('--main-color', `${color}`);
 }
-function changeImg(src){
+function changeImg(src) {
     setTimeout(() => {
-        mainImg.src=src
+        mainImg.src = src
     }, 200);
 }
-function changeThumbs(thumb){
-    thumbs.forEach(thumb2=>{
-        if(thumb2.classList="active"){
+function changeThumbs(thumb) {
+    thumbs.forEach(thumb2 => {
+        if (thumb2.classList = "active") {
             thumb2.classList.remove("active")
         }
     })
     thumb.classList.add("active")
 }
 thumbs.forEach(thumb => {
-    thumb.addEventListener("click",()=>{
+    thumb.addEventListener("click", () => {
         changeThumbs(thumb)
         changeCircle(thumb.dataset.color)
         changeImg(thumb.dataset.src)
     })
 });
+
+
 //change navbar color
-//change navbar color
-window.addEventListener("resize",()=>{
+/* window.addEventListener("resize",()=>{
     if (window.innerWidth < 767.98){
         let navbar = document.querySelector(".navbar")
         navbar.classList.add("navbar-dark")
@@ -35,4 +36,8 @@ window.addEventListener("resize",()=>{
         let navbar = document.querySelector(".navbar")
         navbar.classList.remove("navbar-dark")
     }
-})
+}) */
+if (window.matchMedia('(max-width: 767.98px)').matches) {
+    let navbar = document.querySelector(".navbar")
+    navbar.classList.add("navbar-dark")
+}
